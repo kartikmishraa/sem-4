5176f4adfc7cca4dd2996cb3e3e418e781ed4351
Kartik Mishra <40682913+kartikmishraa@users.noreply.github.com>
2022-04-03 21:37:31 +0530

    Add files via upload

---
+++ lab4/ques1.java
@@ -0,0 +1,5 @@
+class ques1{
+  public static void main(String args[]){
+    System.out.println(args[0]);
+  }
+}
---
+++ lab4/ques2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class ques2{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter name: ");
+    String name = sc.nextLine();
+
+    System.out.println("Name: " + name);
+  }
+}
